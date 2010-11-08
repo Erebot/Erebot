c4afc18c42137c4ab9e026eb2bc1749cf12c1795
François Poirotte <clicky@erebot.net>
2010-11-08 21:45:59 +0000

    Even closer to a pear package...

---
+++ packagexmlsetup.php
@@ -0,0 +1,22 @@
+<?php
+
+$package->license = 'GPL';
+$compatible->license = 'GPL';
+
+/**
+ * Extra package.xml settings such as dependencies.
+ * More information: http://pear.php.net/manual/en/pyrus.commands.make.php#pyrus.commands.make.packagexmlsetup
+ */
+/**
+ * for example:
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
+$package->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
+$package->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
+$package->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
+
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Autoload']->save();
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_Exception']->save();
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_MultiErrors']->save();
+$compatible->dependencies['required']->package['pear2.php.net/PEAR2_HTTP_Request']->save();
+*/
+?>
