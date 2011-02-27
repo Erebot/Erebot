e59c20c08d6e4d88676bd729a9b7287999296600
François Poirotte <clicky@erebot.net>
2011-02-27 16:40:57 +0000

    Use a data provider to make the test more readable.

--- tests/src/utilsTest.php
+++ tests/src/utilsTest.php
@@ -78,115 +78,131 @@ extends PHPUnit_Framework_TestCase
         $this->assertEquals($extracted, Erebot_Utils::extractNick($extracted));
     }
 
-    public function testIsUTF8()
+    public function invalidUTF8Sequences()
+    {
+        // Known invalid sequences, taken from Wikipedia:
+        // http://en.wikipedia.org/wiki/UTF-8#Invalid_byte_sequences
+        $sequences = array(
+            // Overlong sequences for U+0020 (SPACE).
+            "\xC0\x20",
+            "\xC1\x20",
+
+            // Invalid continuation code points.
+            "\xED\xA0",
+            "\xED\xA1",
+            "\xED\xA2",
+            "\xED\xA3",
+            "\xED\xA4",
+            "\xED\xA5",
+            "\xED\xA6",
+            "\xED\xA7",
+            "\xED\xA8",
+            "\xED\xA9",
+            "\xED\xAA",
+            "\xED\xAB",
+            "\xED\xAC",
+            "\xED\xAD",
+            "\xED\xAE",
+            "\xED\xAF",
+            "\xED\xB0",
+            "\xED\xB1",
+            "\xED\xB2",
+            "\xED\xB3",
+            "\xED\xB4",
+            "\xED\xB5",
+            "\xED\xB6",
+            "\xED\xB7",
+            "\xED\xB8",
+            "\xED\xB9",
+            "\xED\xBA",
+            "\xED\xBB",
+            "\xED\xBC",
+            "\xED\xBD",
+            "\xED\xBE",
+            "\xED\xBF",
+
+            "\xF4\x90",
+            "\xF4\x91",
+            "\xF4\x92",
+            "\xF4\x93",
+            "\xF4\x94",
+            "\xF4\x95",
+            "\xF4\x96",
+            "\xF4\x97",
+            "\xF4\x98",
+            "\xF4\x99",
+            "\xF4\x9A",
+            "\xF4\x9B",
+            "\xF4\x9C",
+            "\xF4\x9D",
+            "\xF4\x9E",
+            "\xF4\x9F",
+            "\xF4\xA0",
+            "\xF4\xA1",
+            "\xF4\xA2",
+            "\xF4\xA3",
+            "\xF4\xA4",
+            "\xF4\xA5",
+            "\xF4\xA6",
+            "\xF4\xA7",
+            "\xF4\xA8",
+            "\xF4\xA9",
+            "\xF4\xAA",
+            "\xF4\xAB",
+            "\xF4\xAC",
+            "\xF4\xAD",
+            "\xF4\xAE",
+            "\xF4\xAF",
+            "\xF4\xB0",
+            "\xF4\xB1",
+            "\xF4\xB2",
+            "\xF4\xB3",
+            "\xF4\xB4",
+            "\xF4\xB5",
+            "\xF4\xB6",
+            "\xF4\xB7",
+            "\xF4\xB8",
+            "\xF4\xB9",
+            "\xF4\xBA",
+            "\xF4\xBB",
+            "\xF4\xBC",
+            "\xF4\xBD",
+            "\xF4\xBE",
+            "\xF4\xBF",
+
+            // Invalid starting bytes.
+            "\xF5",
+            "\xF6",
+            "\xF7",
+            "\xF8",
+            "\xF9",
+            "\xFA",
+            "\xFB",
+            "\xFC",
+            "\xFD",
+            "\xFE",
+            "\xFF",
+        );
+        $result = array();
+        foreach ($sequences as $sequence)
+            $result[] = array($sequence);
+        return $result;
+    }
+
+    /**
+     * @dataProvider invalidUTF8Sequences
+     */
+    public function testIsUTF8($sequence)
+    {
+        $this->assertFalse(Erebot_Utils::isUTF8($sequence));
+    }
+
+    public function testIsUTF8SimpleSequences()
     {
         // U+00E9 (LATIN SMALL LETTER E WITH ACUTE) - UTF-8 encoded.
         $this->assertTrue(Erebot_Utils::isUTF8("\xC3\xA9"));
         // Same character, but encoded using iso-8859-1.
         $this->assertFalse(Erebot_Utils::isUTF8("\xE9"));
-        
-        // Known invalid sequences, taken from Wikipedia:
-        // http://en.wikipedia.org/wiki/UTF-8#Invalid_byte_sequences
-
-        // Overlong sequences for U+0020 (SPACE).
-        $this->assertFalse(Erebot_Utils::isUTF8("\xC0\x20"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xC1\x20"));
-
-        // Invalid continuation code points.
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA0"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA1"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA2"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA3"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA4"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA5"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA6"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA7"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA8"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xA9"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAA"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAB"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAC"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAD"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAE"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xAF"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB0"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB1"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB2"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB3"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB4"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB5"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB6"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB7"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB8"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xB9"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBA"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBB"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBC"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBD"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBE"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xED\xBF"));
-
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x90"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x91"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x92"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x93"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x94"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x95"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x96"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x97"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x98"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x99"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9A"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9B"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9C"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9D"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9E"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\x9F"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA0"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA1"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA2"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA3"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA4"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA5"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA6"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA7"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA8"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xA9"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAA"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAB"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAC"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAD"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAE"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xAF"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB0"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB1"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB2"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB3"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB4"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB5"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB6"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB7"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB8"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xB9"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBA"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBB"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBC"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBD"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBE"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF4\xBF"));
-
-        // Invalid starting bytes.
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF5"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF6"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF7"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF8"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xF9"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFA"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFB"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFC"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFD"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFE"));
-        $this->assertFalse(Erebot_Utils::isUTF8("\xFF"));
     }
 
     public function testToUTF8()
