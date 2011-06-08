0771d18015faa7014f149c3688c68ec534f47227
François Poirotte <clicky@erebot.net>
2011-06-08 22:26:58 +0000

    Fix URI parsing for IPv6 addresses.
    The Erebot_URI class incorrectly parsed IPv6 addresses
    when a port was specified (eg. "http://[::1]:80/").
    Also, the pattern for IPv6 addresses was incorrect.
    Added a new set of tests for host parsing (hostname,
    IPv4 & IPv6).

--- tests/src/URITest.php
+++ tests/src/URITest.php
@@ -193,3 +193,49 @@ extends PHPUnit_Framework_TestCase
         }
     }
 }
+
+class   HostTest
+extends PHPUnit_Framework_TestCase
+{
+    public function validProvider()
+    {
+        $values = array();
+        $data   = array(
+            '[2a01:e35:2e30:c120::28]',                 // irc.secours.iiens.net
+            '[2a01:e34:ee8f:6730:201:2ff:fe01:e964]',   // erebot.net
+            '[::1]',                                    // ip6-loopback
+            "localhost",
+            "127.0.0.1",
+        );
+        foreach ($data as $host)
+            $values[] = array($host);
+        return $values;
+    }
+
+    /**
+     * @dataProvider validProvider
+     */
+    public function testNoPort($host)
+    {
+        try {
+            $uri = new Erebot_URI('http://'.$host.'/');
+        }
+        catch (Erebot_InvalidValueException $e) {
+            $this->fail("'".$host."' and no port: ".$e->getMessage());
+        }
+    }
+
+    /**
+     * @dataProvider validProvider
+     */
+    public function testWithPort($host)
+    {
+        try {
+            $uri = new Erebot_URI('http://'.$host.':42/');
+        }
+        catch (Erebot_InvalidValueException $e) {
+            $this->fail("'".$host."' and a port: ".$e->getMessage());
+        }
+    }
+}
+
