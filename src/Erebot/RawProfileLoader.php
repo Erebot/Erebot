1e82559c492afb880050d19feb36c68cadda410c
François Poirotte <clicky@erebot.net>
2011-06-30 20:11:08 +0000

    Remove dead code.

--- src/Erebot/RawProfileLoader.php
+++ src/Erebot/RawProfileLoader.php
@@ -54,54 +54,6 @@ implements  Erebot_Interface_RawProfileLoader,
         foreach ($profiles as $profile) {
             $this->_profiles[] = $this->_getReflect($profile);
         }
-
-#        if (!interface_exists($profile))
-#            throw new Erebot_InvalidValueException('Not an interface');
-
-#        $reflector = new ReflectionClass($profile);
-#        if (!$reflector->implementsInterface('Erebot_Interface_RawProfile'))
-#            throw new Erebot_InvalidValueException(
-#                '$profile must inherit from "Erebot_Interface_RawProfile"'
-#            );
-
-#        $raws               = array();
-#        $ifaces             = $reflector->getInterfaces();
-#        $ifaces[$profile]   = $reflector;
-
-#        foreach ($ifaces as $iface_name => $iface) {
-#            /* There is no direct way to get the constants defined in
-#             * the interface without also retrieving inherited constants.
-#             * So we have to take care of the filtering ourselves. */
-#            $constants = $iface->getConstants();
-#            foreach ($iface->getInterfaces() as $parent_iface)
-#                $constants = array_diff($constants, $parent_iface->getConstants());
-
-#            foreach (array_keys($constants) as $const_name) {
-#                // We use the reflector to get the value to support overloading.
-#                $const_value = $reflector->getConstant($const_name);
-
-#                if (!is_int($const_value) || $const_value < 0 || $const_value > 999)
-#                    continue;
-
-#                /* We look for two constants defined with the same value.
-#                 * To allow aliases, two constants with the same value defined
-#                 * in the same interface are treated as not conflicting. */
-#                if (isset($raws[$const_value]) &&
-#                    !isset($raws[$const_value][$iface_name]) &&
-#                    count($raws[$const_value]) > 1) {
-#                    reset($raws[$const_value]);
-#                    $remote_iface = key($raws[$const_value]);
-#                    $remote_const = $raws[$const_value][$remote_iface][0];
-#                    throw new Erebot_InvalidValueException(
-#                        "Definition of $iface::$const_name conflicts ".
-#                        "with that of $remote_iface::$remote_const"
-#                    );
-#                }
-#                $raws[$const_value][$iface_name][] = $const_name;
-#            }
-#        }
-
-#        $this->_profile = $reflector;
     }
 
     public function getProfiles()
@@ -129,15 +81,6 @@ implements  Erebot_Interface_RawProfileLoader,
             return $const_value;
         }
         return NULL;
-
-#        if (!$this->_profile->hasConstant($rawName))
-#            return NULL;
-
-#        $const_value = $this->_profile->getConstant($rawName);
-#        if (!is_int($const_value) || $const_value < 0 || $const_value > 999)
-#            return NULL;
-
-#        return $const_value;
     }
 
     /**
