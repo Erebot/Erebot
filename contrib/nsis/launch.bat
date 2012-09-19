170538c9db9ab7923ffc6d288ec05639c8e385e5
François Poirotte <clicky@erebot.net>
2012-09-19 20:58:15 +0200

    Improve launcher.
    Disable detect_unicode as it causes issues with phar files.

--- contrib/nsis/launch.bat
+++ contrib/nsis/launch.bat
@@ -5,7 +5,7 @@ rem The block is necessary to workaround issues in cmd.exe
 rem when running with codepage 65001 (UTF-8).
 (
     chcp 65001 > NUL
-    php.exe -f Erebot.phar
+    php.exe -d detect_unicode=Off -f Erebot.phar
     chcp 850 > NUL
     echo Press Ctrl+C to exit...
     pause > NUL
