dd6c9757563bdc9f10b5d1925f2380dfe2a50ef6
lefa <aki.kulju@gmail.com>
2012-09-29 15:38:13 +0300

    Update src/Erebot/Prompt.php
    Erebot_Prompt::_handleMessage was trying to call undefined Erebot_IrcConnection::pushLine(). Replaced the call by Erebot_IrcConnection::getUI()::push().

--- src/Erebot/Prompt.php
+++ src/Erebot/Prompt.php
@@ -233,7 +233,7 @@ implements  Erebot_Interface_ReceivingConnection
             $config = $connection->getConfig(NULL);
             $netConfig = $config->getNetworkCfg();
             if (preg_match('@^'.$pattern.'$@Di', $netConfig->getName()))
-                $connection->pushLine($line);
+                $connection->getIO()->push($line);
         }
     }
 
