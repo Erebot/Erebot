82adb06c79412cfa8ef0211c2fbec9c2b8874786
François Poirotte <clicky@erebot.net>
2013-07-29 23:38:49 +0200

    Update stub following composer migration.

--- stub.php
+++ stub.php
@@ -17,32 +17,27 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-if (!empty($_SERVER['DOCUMENT_ROOT']))
-    die("This script isn't meant to be run from the Internet!\n");
-
 if (realpath($_SERVER['PATH_TRANSLATED']) == realpath(__FILE__))
     // Don't use any external file
     // (eg. from a PEAR repository).
-    ini_set('include_path', PATH_SEPARATOR);
+    ini_set('include_path', '.');
 
 if (version_compare(phpversion(), '5.3.1', '<')) {
     if (substr(phpversion(), 0, 5) != '5.3.1') {
         // this small hack is because of running RCs of 5.3.1
-        echo "@PACKAGE_NAME@ requires PHP 5.3.1 or newer." . PHP_EOL;
+        echo basename(__FILE__) . " requires PHP 5.3.1 or newer." . PHP_EOL;
         exit(1);
     }
 }
 
 // Check dependencies needed to parse the phar.
-$exts = array('phar', 'spl', 'pcre', 'simplexml');
+$exts = array('phar', 'json');
 foreach ($exts as $ext) {
     if (!extension_loaded($ext)) {
         echo "Extension $ext is required." . PHP_EOL;
         exit(1);
     }
 }
-unset($exts, $ext);
-
 
 // Parse the current phar.
 try {
@@ -52,36 +47,6 @@ try {
     echo "\t" . $e->getMessage() . PHP_EOL;
     exit(1);
 }
-
-// Load the autoloader.
-include(
-    "phar://" . __FILE__ .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
-    DIRECTORY_SEPARATOR . "php" .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@" .
-    DIRECTORY_SEPARATOR . "Autoload.php"
-);
-
-// Register composer into the autoloader.
-$composerDir =
-    "phar://" . __FILE__ .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
-    DIRECTORY_SEPARATOR . "php";
-Erebot_Autoload::initialize($composerDir);
-unset($composerDir);
-
-// Include the dependency checker.
-include(
-    "phar://" . __FILE__ .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
-    DIRECTORY_SEPARATOR . "data" .
-    DIRECTORY_SEPARATOR . "pear.erebot.net" .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@" .
-    DIRECTORY_SEPARATOR . "checker.php"
-);
-
-// Prepare a new dependency checker.
-$checker = new Erebot_Package_Dependencies_Checker("", '@PACKAGE_VERSION@');
 
 // Use closures to avoid variables pollution.
 $inc = function ($modulePath) {
@@ -97,50 +62,69 @@ $inc = function ($modulePath) {
     }
     return $res;
 };
-$phars      = array();
-$handleMetadata = function ($checker, $metadata, &$phars, $pharPath) {
-    $checker->handleMetadata($metadata);
 
-    list($vendor, $pkgName) = explode('/', $metadata['name'], 2);
-    if ($vendor == 'erebot' || $vendor == 'pear.erebot.net') {
-        if (!isset($metadata['extra']['phar']['path']) ||
-            !isset($metadata['extra']['PEAR']['name'])) {
-            return;
-        }
-        $path       = $metadata['extra']['phar']['path'];
-        $pearName   = $metadata['extra']['PEAR']['name'];
-        $phars[$pearName]['version'] = $metadata['version'];
-        if (file_exists($path)) {
-            $phars[$pearName]['paths'][] = $path;
-        }
+// Load the autoloader.
+include(
+    "phar://" . __FILE__ .
+    DIRECTORY_SEPARATOR . "vendor" .
+    DIRECTORY_SEPARATOR . "erebot" .
+    DIRECTORY_SEPARATOR . "api" .
+    DIRECTORY_SEPARATOR . "src" .
+    DIRECTORY_SEPARATOR . "Erebot" .
+    DIRECTORY_SEPARATOR . "Autoload.php"
+);
+
+// Register the path to Erebot's core files into the autoloader.
+Erebot_Autoload::initialize(
+    "phar://" . __FILE__ .
+    DIRECTORY_SEPARATOR . "src"
+);
+
+// Now register dependencies too.
+$metadata = json_decode(
+    file_get_contents('phar://' . __FILE__ . '/composer.lock'),
+    TRUE
+);
+$packages = array_merge($metadata['packages'], $metadata['packages-dev']);
+
+// First pass : register autoload paths.
+foreach ($packages as $package) {
+    if (!file_exists('phar://' . __FILE__ . "/vendor/${package['name']}"))
+        continue;
+
+    if (!isset($package['autoload']['psr-0']))
+        continue;
+
+    foreach ($package['autoload']['psr-0'] as $autoload) {
+        Erebot_Autoload::initialize(
+            "phar://" . __FILE__ . "/vendor/${package['name']}/$autoload"
+        );
     }
-};
+}
+
+$repository = new \Composer\Repository\InstalledArrayRepository();
+$loader = new \Composer\Package\Loader\ArrayLoader();
+
+// Second pass : register bundled packages.
+foreach ($packages as $package) {
+    // erebot/generic-doc uses a different install-dir.
+    if ($package['name'] !== 'erebot/generic-doc' &&
+        !file_exists('phar://' . __FILE__ . "/vendor/${package['name']}"))
+        continue;
+    $repository->addPackage($loader->load($package));
+}
 
 $metadata = json_decode(
     file_get_contents(
         "phar://" . __FILE__ .
-        DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
-        DIRECTORY_SEPARATOR . "data" .
-        DIRECTORY_SEPARATOR . "pear.erebot.net" .
-        DIRECTORY_SEPARATOR . "@PACKAGE_NAME@" .
         DIRECTORY_SEPARATOR . "composer.json"
     ),
     TRUE
 );
-// Add the extra info the normal stub would usually add.
-$metadata['version'] = '@PACKAGE_VERSION@';
-$metadata['extra']['PEAR']['name'] = '@PACKAGE_NAME@';
-$metadata['extra']['phar']['path'] =
-    "phar://" . __FILE__ .
-    DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
-    DIRECTORY_SEPARATOR . "php";
-// Erebot's main .phar embeds the code of several dependencies.
-$metadata['provide']['erebot/erebot-api'] = '*';
-$metadata['provide']['erebot/plop'] = '*';
-$metadata['provide']['erebot/dependencyinjection'] = '*';
-$metadata['provide']['pear-pear.php.net/console_commandline'] = '*';
-$metadata['provide']['pear-pear.php.net/file_gettext'] = '*';
-$handleMetadata($checker, $metadata, $phars, __FILE__);
+$phar = new Phar(__FILE__);
+$md = $phar->getMetadata();
+$metadata['version'] = $md['version'];
+$repository->addPackage($loader->load($metadata));
 
 // Load phar modules.
 $modulesDir = getcwd() . DIRECTORY_SEPARATOR . 'modules';
@@ -151,14 +135,38 @@ try {
         if (in_array($moduleInfo->getFilename(), $dots))
             continue;
 
-        if (substr($moduleInfo->getFilename(), -5) != '.phar')
+        if (substr($moduleInfo->getFilename(), -5) !== '.phar')
             continue;
 
         try {
             // Take the module's metadata into account.
             $pharPath = $moduleInfo->getPathName();
+
+            ob_start();
             $metadata = $inc($pharPath);
-            $handleMetadata($checker, $metadata, $phars, $pharPath);
+            $buffer = ob_get_clean();
+
+            $cut = strpos($buffer, "\r");
+            if ($cut === FALSE)
+                $cut = strpos($buffer, "\n");
+            else if (strpos($buffer, "\n") !== FALSE)
+                $cut = min($cut, strpos($buffer, "\n"));
+
+            $shebang = (string) substr($buffer, 0, $cut);
+            $buffer = (string) substr($buffer, $cut);
+
+            if (substr($shebang, 0, 2) === '#!') {
+                if (substr($buffer, 0, 1) === "\r")
+                    $buffer = (string) substr($buffer, 1);
+                if (substr($buffer, 0, 1) === "\n")
+                    $buffer = (string) substr($buffer, 1);
+            }
+            echo $buffer;
+
+            $phar = new Phar($pharPath);
+            $md = $phar->getMetadata();
+            $metadata['version'] = $md['version'];
+            $repository->addPackage($loader->load($metadata));
         }
         catch (Exception $e) {
         }
@@ -167,23 +175,36 @@ try {
 catch (Exception $e) {
 }
 
-// Define constants with the paths to each module's phars (+ core).
-define('Erebot_PHARS', serialize($phars));
+// create installed repo, this contains
+// all local packages + platform packages (php, extensions & libs)
+$platformRepository = new \Composer\Repository\PlatformRepository();
+$installedRepository = new \Composer\Repository\CompositeRepository(array(
+    $platformRepository,
+    $repository,
+));
 
-// Global scope cleanup.
-unset(
-    $metadata, $modulesDir, $iter, $dots,
-    $moduleInfo, $inc, $pkgName, $phars,
-    $pharPath
+$pool = new \Composer\DependencyResolver\Pool;
+$pool->addRepository($platformRepository);
+$pool->addRepository($repository);
+
+$request = new \Composer\DependencyResolver\Request($pool);
+$phar = new Phar(__FILE__);
+$md = $phar->getMetadata();
+
+$request->install("erebot/erebot");
+$policy = new \Composer\DependencyResolver\DefaultPolicy();
+$solver = new \Composer\DependencyResolver\Solver(
+    $policy,
+    $pool,
+    $repository
 );
 
-// Check the dependencies.
-$error = NULL;
-if (!$checker->check($error)) {
-    echo $error;
+try {
+    $solver->solve($request);
+} catch (\Composer\DependencyResolver\SolverProblemsException $e) {
+    echo $e->getMessage() . PHP_EOL;
     exit(1);
 }
-unset($error, $checker);
 
 // Necessary for PEAR 1 packages that require() stuff.
 set_include_path(implode(PATH_SEPARATOR, Erebot_Autoload::getPaths()));
